150b388a79c2d756cd0fa6f370fc3d46ff6e341d
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2024-08-12 10:34:18 +0000

    http server completed

--- http_header.js
+++ http_header.js
@@ -1,19 +1,39 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+//get all files
+
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const httpHeader = () => {
   const server = http.createServer((req, res) => {
     const url = req.url;
     if (url === "/") {
       res.writeHead(200, { "content-type": "text/html" });
-      res.write("<h1>Home Page</h1>");
+      res.write(homePage);
       res.end();
     } else if (url === "/about") {
       res.writeHead(200, { "content-type": "text/html" });
       res.write("<h1>About Page</h1>");
       res.end();
+    } else if ( url === '/styles.css') {
+      res.writeHead(200, { 'content-type': "text/css"})
+      res.write(homeStyles)
+      res.end()
+    } else if ( url === '/browser-app.js') {
+      res.writeHead(200, { 'content-type': 'text/javascript'})
+      res.write(homeLogic)
+      res.end()
+    } else if (url === '/logo.svg') {
+      res.writeHead(200, { 'content-type': 'image/svg+xml'})
+      res.write(homeImage)
+      res.end()
     } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.end();
+      res.writeHead(404, { "content-type": "text/html" });
+      res.end("<h1>Page not found</h1>");
     }
   });
 
